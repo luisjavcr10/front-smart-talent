07722919bd0d6ed8dc46033f91edede21e3d9dd4
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-04-29 22:47:19 -0500

    add second login version

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -2,26 +2,27 @@ import GoogleLogo from "../../shared/ui/logos/GoogleLogo";
 
 export function LoginPage() {
     return (
-      <main className="bg-black-0 flex justify-center items-center h-screen w-full font-display">
-        <div className="bg-black-1 text-white h-screen w-full flex flex-col justify-center items-center border-r-[5px] border-r-orange rounded-r-5xl">
+      <main className="bg-black-0 flex justify-center items-center h-screen w-full font-bevietnampro font-light">
+        <div className="bg-black-1 text-white h-screen w-9/20 flex flex-col justify-center items-center border-r-[5px] border-r-orange rounded-r-5xl">
             <img src="/logo-black.png" alt="logo" className="mb-10"/>
-            <h1 className="text-[48px]">HOLA DE NUEVO!!</h1>
-            <p className="text-[16px] cursor-pointer">Aun no estas registrado?</p>
+            <h1 className="text-[48px] font-bebasneue font-normal">¡HOLA DE NUEVO!</h1>
+            <p className="text-[16px] cursor-pointer hover:text-orange opacity-80">¿Aún no tienes cuenta? Registrate aquí</p>
         </div>
-         <div className="text-white h-screen w-full flex flex-col justify-center items-center">
+         <div className="text-white h-screen w-11/20 flex flex-col justify-center items-center">
             <div className="flex flex-col justify-start w-3/5 gap-4">
                 <p className="w-full opacity-50 text-[20px]">Por favor ingresa tus datos</p>
                 <form className="w-full flex flex-col items-start gap-6">
                     <div className="flex flex-col w-full gap-4">
                         <label className="text-[20px]">Email</label>
-                        <input type="email" className="bg-white-20 rounded-[15px] h-10 px-4 py-2" />
+                        <input type="email" className="bg-white-20 rounded-[15px] h-10 px-4 py-2 outline-orange" />
                     </div>
                     <div className="flex flex-col w-full gap-4">
                         <label className="text-[20px]">Contraseña</label>
-                        <input type="password" className="bg-white-20 rounded-[15px] h-10 px-4 py-2" />
+                        <input type="password" className="bg-white-20 rounded-[15px] h-10 px-4 py-2 outline-orange" />
+                        <a className="opacity-50 text-[16px] cursor-pointer hover:text-orange hover:opacity-80">¿Te olvidaste tu contraseña?</a>
                     </div>
-                    <a className="opacity-50 text-[16px] cursor-pointer">Olvidaste tu contraseña?</a>
-                    <button className="bg-orange hover:opacity-80 rounded-[15px] px-4 py-2 w-full mt-6 cursor-pointer text-[24px]">Ingresar</button>
+                    
+                    <button className="bg-orange hover:opacity-80 rounded-[15px] px-4 py-2 w-full mt-6 cursor-pointer text-[20px]">Ingresar</button>
                 </form>
                 <button 
                     className="
@@ -33,7 +34,7 @@ export function LoginPage() {
                         cursor-pointer
                         gap-4">
                     <GoogleLogo className="h-[30px] w-[30px]"/>
-                    <p className="text-[24px]">Ingresar con Google</p>
+                    <p className="text-[20px]">Ingresar con Google</p>
                 </button>
             </div>
         </div>
