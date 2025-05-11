4f6d1cfaf2f7ae19bac8f39e9bc24324202091b2
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-05-11 04:32:02 -0500

    feat(requests): add requests creation page and route
    Introduce a new page for creating requests and add a corresponding route. The page includes a table displaying request details such as DNI, full name, and document statuses.

--- src/requests/requestsRoutes.tsx
+++ src/requests/requestsRoutes.tsx
@@ -1,9 +1,14 @@
 import { RequestsListPage } from "./pages/RequestsListPage";
+import { RequestsCreationPage } from "./pages/RequestsCreationPage";
 
 export const requestsRoutes = [
   {
     path: '/requests',
     element: <RequestsListPage />,
+  },
+  {
+    path:'/requests/create',
+    element: <RequestsCreationPage />
   },
   {
     path:'/'
